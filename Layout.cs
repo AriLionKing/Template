565027fa7e3174fa9d465ca67ac01545462d8078
AriLionKing <arims12345@gmail.com>
2019-10-10 11:26:18 -0600

    Add files via upload

---
+++ Layout.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace Layout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //files
+
+            //files
+            //int
+
+            //int
+            //var
+            var loop = true;
+
+            //var
+            //string
+            string command;
+
+            //string
+            //function
+           
+            //function
+            
+            while(loop == true)
+            {
+                Console.Write(">>");
+                command = Console.ReadLine();
+                switch(command)
+                {
+                    
+                    case "end":
+                    loop = false;
+                    break;
+                    default:
+                    Console.WriteLine("Invalid Command");
+                    break;
+                }
+            }
+        }
+    }
+}
